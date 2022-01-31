192db088dc4eee0c6fbbb02a089135577332dbbc
Mahan Ziyari <maziyar.mz.021@gmail.com>
2022-01-31 12:11:59 +0330

    Initial commit

---
+++ app/src/main/java/com/mahan/compose/jettodo/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.mahan.compose.jettodo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.mahan.compose.jettodo.ui.theme.JetTodoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoApp {
+                
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoApp(content: @Composable() ()-> Unit) {
+    JetTodoTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TodoApp {
+
+    }
+}
